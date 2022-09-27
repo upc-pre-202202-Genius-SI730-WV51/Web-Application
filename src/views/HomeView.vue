83cc322cf540e0a521e1657a6f74f2d39205cedd
PolVega <u20201b333@upc.edu.pe>
2022-09-27 17:02:18 -0500

    feat/UserProfile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,24 @@
 <template>
   <main>
     <h1>Welcome to FreePlace</h1>
+
+    <div>
+      <image src="image.png" alt="Image" width="120px"/>
+      <h2>Preview Content</h2>
+
+    </div>
   </main>
+
 </template>
 
 <style>
 h1{
   color:black;
   text-align:center;
+}
+h2{
+  color:black;
+  background-size:auto;
+  background: purple;
 }
 </style>
